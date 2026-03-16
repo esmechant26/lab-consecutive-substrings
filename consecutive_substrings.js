d8778fe5d5953fe6d35a9ccbbdb905d664c3fb9f
Esme Chant <esmemchant@gmail.com>
2026-03-16 18:38:03 -0400

    project completed

--- consecutive_substrings.js
+++ consecutive_substrings.js
@@ -1,18 +1,27 @@
 function consecutiveSubstrings(string) {
-  // type your code here
+  let arr = [];
+  for (let i = 0; i < string.length; i++) {
+    let current = "";
+    for (let j = i; j < string.length; j++) {
+      current += string[j];
+      arr.push(current);
+    }
+  }
+  return arr;
 }
 
 if (require.main === module) {
   // add your own tests in here
   console.log("Expecting: ['a', 'ab', 'abc', 'b', 'bc', 'c']");
-  console.log("=>", consecutiveSubstrings('abc'));
+  console.log("=>", consecutiveSubstrings("abc"));
+
+  console.log("Expecting: ['e', 'es', 'esm', 'esme', 's', 'sm', 'sme', 'm', 'me', 'e']");
+  console.log("=>", consecutiveSubstrings("esme"));
 
   console.log("");
 
   console.log("Expecting: ['a']");
-  console.log("=>", consecutiveSubstrings('a'));
+  console.log("=>", consecutiveSubstrings("a"));
 }
 
 module.exports = consecutiveSubstrings;
-
-
